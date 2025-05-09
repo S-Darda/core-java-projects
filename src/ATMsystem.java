9b44b425790e12e8cdb21fd3dcd37843a8fa7e06
s-darda <darda.rahman52@gmail.com>
2025-05-09 22:48:26 +0100

    Accounnt info and Blance methods added

--- src/ATMsystem.java
+++ src/ATMsystem.java
@@ -7,8 +7,8 @@ public class ATMsystem {
     Scanner input =new Scanner(System.in);
     HashMap<Integer, User> users = new HashMap<>();
     void initializeUsers() {
-        users.put(12345, new User(1111, 500.0));
-        users.put(67890, new User(2222, 300.0));
+        users.put(12345, new User(1111, 500.0,"Darda"));
+        users.put(67890, new User(2222, 300.0,"Erada"));
     }
     void display()
     {
@@ -22,6 +22,7 @@ public class ATMsystem {
         System.out.println("|--------------------|");
         if(authenticate(acNum,pin)){
             System.out.println("Login Successful");
+            operation(acNum);
         }
         else {
             System.out.println("login Failed");
@@ -36,21 +37,52 @@ public class ATMsystem {
         return user != null && user.pin == pin;
 
     }
+    void operation(int accNum)
+    {
+        User user = users.get(accNum);
+       do {
+           System.out.println("-------welcome---------");
+           System.out.println("1. Account Info");
+           System.out.println("2. Balance");
+           System.out.println("3. Deposit");
+           System.out.println("4. Withdraw");
+           System.out.println("5. Logout");
+           System.out.println("#. Enter your Choice :");
+           int choice = input.nextInt();
+           switch (choice){
+               case 1:
+                   AcountInfo(accNum);
+                   break;
+               case 2:
+                   Blance(accNum);
+                   break;
+           }
+       }
+        while(true);
+    }
+    void AcountInfo(int accNum)
+    {
+        User user = users.get(accNum);
+        System.out.println("<---Account Info--->");
+        System.out.println("Name : "+ user.name);
+        System.out.println("Balance :"+user.balance);
+        System.out.println("Pin :"+user.pin);
+    }
+    void Blance(int accNum){
+        User user = users.get(accNum);
+        System.out.println("--------------------");
+        System.out.println("Your Current Balance :"+user.balance);
+        System.out.println("--------------------");
+
+    }
 
     public static void main(String[] args) {
         ATMsystem atMsystem =new ATMsystem();
         atMsystem.initializeUsers();
         atMsystem.display();
     }
-    class User {
-        int pin;
-        double balance;
 
-        User(int pin, double balance) {
-            this.pin = pin;
-            this.balance = balance;
-        }
     }
 
-}
+
 
---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+        int pin;
+        double balance;
+        String name;
+
+        User(int pin, double balance,String name) {
+            this.pin = pin;
+            this.balance = balance;
+            this.name =name;
+        }
+}
